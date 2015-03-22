2b54c4bf225360b5f1a9fb44a3b2894b2827ae5e
roberthan <roberthan818@gmail>
2015-03-22 11:40:36 -0700

    meh

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls import patterns, include, url
-from mysite.glassroot import views
+# from mysite.glassroot import views
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'mysite.glassroot.views.home', name='home'),
+    url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^$', include('mysite.glassroot.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
