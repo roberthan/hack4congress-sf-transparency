01f4213886047715553d086b3f7ebf8acca73046
roberthan <roberthan818@gmail>
2015-03-22 11:35:12 -0700

    meh

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-import mysite.glassroot
+import glassroot
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
