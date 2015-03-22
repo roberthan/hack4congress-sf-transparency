c1ab6dc6e9103c57a9a7bf62cb95b9c73956b8b6
roberthan <roberthan818@gmail>
2015-03-22 11:48:53 -0700

    meh

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -110,8 +110,8 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    '/home/ec2-user/hack4congress/mysite/templates', # Change this to your own directory.  
 )
-
 INSTALLED_APPS = (
     'django.contrib.auth',
     'django.contrib.contenttypes',
