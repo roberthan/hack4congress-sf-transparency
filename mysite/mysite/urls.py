1cba31dc1c6ddca3dd640c79a0b23fc1ebc594d2
roberthan <roberthan818@gmail>
2015-03-22 11:30:31 -0700

    meh

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    url(r'^$', 'mysite.views.home', name='home')
+    url(r'^$', 'mysite.views.home', name='home'),
     # url(r'^$', include('mysite.glassroot.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
