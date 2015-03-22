f7b69cfb37a039ace73745e53a50ee20df8e735c
Ranna Zhou <rannazhou@gmail.com>
2015-03-22 10:38:39 -0700

    models

--- mysite/grassroot/models.py
+++ mysite/grassroot/models.py
@@ -1,4 +1,15 @@
 from django.db import models
 
 # Create your models here.
-class User 
+class Committee(models.Model):
+	name = models.CharField()
+
+class Senator(models.Model):
+    name = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    committee = models.ForeignKey(Committee)
+
+class Bill(models.Model):
+	name = models.CharField()
+	num = models.IntegerField()
+	committee = models.ForeignKey(Committee)
