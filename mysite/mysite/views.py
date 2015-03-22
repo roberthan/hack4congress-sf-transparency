a453414f44a2088835f12c20462cc2ed0c3ae6cd
Ranna Zhou <rannazhou@gmail.com>
2015-03-22 12:15:58 -0700

    ok

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -7,3 +7,15 @@ def home(request):
 	t = loader.get_template('hack4congress_index.html')
 	c = Context()
 	return HttpResponse(t.render(c))
+
+def i2s_process(request):
+	interest = request.GET['id']
+	with open('i2s.json') as i2s_file:
+		i2s = json.load(i2s_file)
+		result = i2s[interest]
+		return HttpResponse(result)
+
+def result(request):
+	t = loader.get_tempalate('hack4congress_results.html')
+	c = Context()
+	return HttpResponse(t.render(c))
