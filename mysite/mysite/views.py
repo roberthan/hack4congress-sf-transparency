72f8b5bfbd4a11a78bd85341afd78b607a841405
Ranna Zhou <rannazhou@gmail.com>
2015-03-22 12:51:17 -0700

    Asdf

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -6,7 +6,7 @@ import datetime
 
 def home(request):
 	t = loader.get_template('hack4congress_index.html')
-	f = [x.strip() for x in open("/home/ec2-user/hack4congress/mysite/mysite/interests.txt").readlines()]
+	f = sorted([x.strip() for x in open("/home/ec2-user/hack4congress/mysite/mysite/interests.txt").readlines()])
 	c = Context({
 		"interests" : f
 		})
@@ -16,7 +16,7 @@ def result(request):
 	interest = request.GET['id']
 	with open('/home/ec2-user/hack4congress/mysite/mysite/i2s.json') as i2s_file:
 		i2s = json.load(i2s_file)
-		result = i2s[interest].strip()
+		result = [x.strip() for x in i2s[interest]]
 	t = loader.get_template('hack4congress_results.html')
 	result = sorted(result, key=lambda x:-x.influence)
 	c = Context({"interestname":interest, "res":result})
